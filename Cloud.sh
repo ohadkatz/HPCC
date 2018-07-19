4f0d683e2145c9e6b10154de349e18157862a2c9
ohadkatz <ohadkatz@buffalo.edu>
2018-07-19 11:32:09 -0400

    Finalized output to 1 file, python script converts to long format post run, changed bash script template

--- Cloud.sh
+++ Cloud.sh
@@ -6,19 +6,14 @@ printf '\e[5t'
 cat ".DGEMMtesting.txt"
 Runs=(2 4 8)
 
-function Reset(){
-    if [ -e hpccoutf.txt ]; then
-        > hpccoutf.txt
-    fi
-    > StarResults.txt
-    > ParallelResults.txt 
-}
-
 function InputReset(){
     sed -i '11s/.*/1/' hpccinf.txt
     sed -i '11s/$/            Ps/' hpccinf.txt
     sed -i "12s/.*/1/" hpccinf.txt
     sed -i '12s/$/            Qs/' hpccinf.txt
+    > Results.txt
+    > FinalResults.txt
+    
 }
 
 function Switch(){
@@ -42,53 +37,55 @@ function Switch(){
         sed -i '12s/$/            Qs/' hpccinf.txt
     fi
 }
-
+Start="0"
+End="0"
 InputReset
-
-printf "===================== PART 2 =========================\n"
+printf "===================== PART 1 =========================\n"
 #=======================Single Node: Serial=================================#
 printf "Serial Run on a Single Node! \n"
+Start=$(wc -l < Results.txt)
 time OMP_NUM_THREADS=1  mpirun -np 1 ./hpcc
-#TURN OFF SCALAPACK??
-cp hpccoutf.txt $OutputDir/Serial/Serialout.txt
-cp StarResults.txt $OutputDir/Serial/SerialResults.txt
-Reset
+End=$(wc -l < Results.txt)
+python IOchange.py 1 1 "Serial" $Start $End
+echo "==============================SERIAL============================" >> hpccoutf.txt
 printf "All set with serial! \n"
 
 #====================Single Node: True Parallel=============================#
 
 printf "True Parallel Run on Single Node! \n"
 for Thread in "${Runs[@]}"; do 
+    Start=$(wc -l < Results.txt)
     time OMP_NUM_THREADS=$Thread  mpirun -np 1 ./hpcc
-    cp hpccoutf.txt $OutputDir/TotParallel/TPout$Thread.txt
-    cp StarResults.txt $OutputDir/TotParallel/TPresults$Thread.txt
-    Reset
+    End=$(wc -l < Results.txt)
+    python IOchange.py 1 $Thread "True Parallel" $Start $End 
+    echo "==========TRUE=======Parallel=========" >> hpccoutf.txt
 done
 printf "Done with True Parallel Run \n"
 printf "True Parallel Run (scaLAPACK) on Single Node! \n"
 
 for P in "${Runs[@]}"; do 
     Switch $P
+    Start=$(wc -l < Results.txt)
     time OMP_NUM_THREADS=1  mpirun -np $P ./hpcc
-    cp hpccoutf.txt $OutputDir/TotParallel/ScaLAPACK/ScaLAPACKTPSPout$P.txt
-    cp ParallelResults.txt $OutputDir/TotParallel/ScaLAPACK/TPSPresults$P.txt
-    Reset
+    End=$(wc -l < Results.txt)
+    python IOchange.py $P 1 "True Parallel(SCALAPACK)" $Start $End 
+    echo "==========True=======Parallel=========SCALAPACK=========" >> hpccoutf.txt
 done
 printf "All set with True Parallel (scaLAPACK) Run\n"
 
 #================Single Node: Embarassingly Parallel========================#
-InputReset
+
 printf "Embarassingly Parallel Run on Single Node!\n"
 for Q in "${Runs[@]}"; do 
     Switch $Q
+    Start=$(wc -l < Results.txt)
     time OMP_NUM_THREADS=1 mpirun -np $Q ./hpcc
-    cp hpccoutf.txt $OutputDir/EmbParallel/EPout$Q.txt
-    cp StarResults.txt $OutputDir/EmbParallel/EPresults$Q.txt
-    Reset
+    End=$(wc -l < Results.txt)
+    python IOchange.py $Q 1 "Embarassingly Parallel" $Start $End 
+    echo "==========Embarassingly=======Parallel=========" >> hpccoutf.txt
 done
-
-InputReset
 printf "Embarassingly Parallel All Done!\n"
 printf "All tests complete! Goodbye.\n"
 printf "To find outputs please go to directory marked outputs! Each portion will have its own Directory \n"
 
+cp FinalResults.txt $OutputDir
